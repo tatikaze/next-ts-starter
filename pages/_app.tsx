790dfe77049c81f80c89dd4e42b8f107886d3bba
Sota Suzuki <k017c1356@it-neec.jp>
2021-03-08 18:02:45 +0900

    feat: add styled-component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 import "../styles/globals.css";
 
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
