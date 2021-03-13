42bf15ec69068b4747b36c723e66b2b43fa2a90f
Sota Suzuki <k017c1356@it-neec.jp>
2021-03-14 00:14:41 +0900

    refact: remove ignorefiles

--- plugins/firebase-key.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyAzI2PCj-V0on13pKWXLxE6ThpucrM7fLs',
-  authDomain: 'next-ts-starter.firebaseapp.com',
-  projectId: 'next-ts-starter',
-  storageBucket: 'next-ts-starter.appspot.com',
-  messagingSenderId: '1073200866005',
-  appId: '1:1073200866005:web:f2ad301dbc2303ad54785c',
-}
