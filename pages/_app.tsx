bf6086cbeb030cd8f4ec0101362f9a1c4c98c8da
Sota Suzuki <hurin@tatikaze.com>
2021-11-23 17:29:56 +0900

    feat: remove styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
 import '../styles/globals.css'
-import { theme } from '~/styles/theme'
 import { RootProvider } from '~/store'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <RootProvider>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </RootProvider>
   )
 }
