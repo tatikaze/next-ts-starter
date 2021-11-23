495b791f9162883b7b621f50acf6b5f30436d169
Sota Suzuki <hurin@tatikaze.com>
2021-11-23 17:35:10 +0900

    feat: add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import { RootProvider } from '~/store'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <RootProvider>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </RootProvider>
   )
 }
